47e8f7d49a4c67288c187f6582b8b609a709e741
tyage <namatyage@gmail.com>
2016-08-19 11:46:44 +0900

    show difference if check failed

--- check.rb
+++ check.rb
@@ -2,3 +2,10 @@ answer = `./bin/jsvm -d #{ARGV[0]}`
 result = `./bin/jsvm -d #{ARGV[0]}c`
 
 puts answer == result
+
+if answer != result
+  puts '==========='
+  puts answer
+  puts '==========='
+  puts result
+end
